b3d971e40bf2673c93b4b7ea6327031d7bccbd35
Ben Szucs <benszucs@gmail.com>
2018-11-26 08:15:35 +0000

    Prototype-based Inheritance

--- playground/index.js
+++ playground/index.js
@@ -75,6 +75,7 @@ var userAccount = new Object ();
 
 
 
+// Prototype Property: Prototype-based Inheritance
 function Plant () {
 this.country = "Mexico";
 this.isOrganic = true;
@@ -82,23 +83,24 @@ this.isOrganic = true;
 
 // Add the showNameAndColor method to the Plant prototype property
 Plant.prototype.showNameAndColor =  function () {
-console.log("I am a " + this.name + " and my color is " + this.color);
+  console.log("I am a " + this.name + " and my color is " + this.color);
 }
 
 // Add the amIOrganic method to the Plant prototype property
 Plant.prototype.amIOrganic = function () {
-if (this.isOrganic)
-console.log("I am organic, Baby!");
+  if (this.isOrganic)
+  console.log("I am organic, Baby!");
 }
 
 function Fruit (fruitName, fruitColor) {
-this.name = fruitName;
-this.color = fruitColor;
+  this.name = fruitName;
+  this.color = fruitColor;
 }
 
 // Set the Fruit's prototype to Plant's constructor, thus inheriting all of Plant.prototype methods and properties.
+console.log("Fruit prototype before inheritance:", Fruit.prototype)
 Fruit.prototype = new Plant ();
-
+console.log("Fruit prototype after inheritance:", Fruit.prototype)
 // Creates a new object, aBanana, with the Fruit constructor
 var aBanana = new Fruit ("Banana", "Yellow");
 
@@ -107,3 +109,4 @@ console.log(aBanana.name); // Banana
 
 // Uses the showNameAndColor method from the Fruit object prototype, which is Plant.prototype. The aBanana object inherits all the properties and methods from both the Plant and Fruit functions.
 console.log(aBanana.showNameAndColor()); // I am a Banana and my color is yellow.
+console.log(aBanana.amIOrganic());
